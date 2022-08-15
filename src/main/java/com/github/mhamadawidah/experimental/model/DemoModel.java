aff0b0f21eedb85f2d8085a5e46918c4dae01ebb
Mhamad Awidah <53232098+mhamadawidah@users.noreply.github.com>
2022-08-15 10:30:59 +0200

    Spring Boot

---
+++ src/main/java/com/github/mhamadawidah/experimental/model/DemoModel.java
@@ -0,0 +1,6 @@
+package com.github.mhamadawidah.experimental.model;
+
+/**
+ * Don't delete!
+ */
+public record DemoModel(int id, String name) {}
---
+++ src/test/java/com/github/mhamadawidah/experimental/ExperimentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mhamadawidah.experimental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExperimentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
