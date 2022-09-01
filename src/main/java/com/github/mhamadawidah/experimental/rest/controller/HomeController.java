3ea27b1a465975913f04859df9221cf3ffdc2908
Mhamad Awidah <53232098+mhamadawidah@users.noreply.github.com>
2022-09-01 09:50:28 +0200

    Thymeleaf

---
+++ src/main/java/com/github/mhamadawidah/experimental/rest/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.github.mhamadawidah.experimental.rest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    private static final String TITLE = "MyOptiks";
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", TITLE);
+        return "home";
+    }
+}
