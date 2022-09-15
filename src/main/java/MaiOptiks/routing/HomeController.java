8d276e82f9a2b21452d7257f1c03c6c4b24bde71
MK <knystockmarten@gmail.com>
2022-09-15 12:37:14 +0200

    Changes

--- src/main/java/MaiOptiks/routing/HomeController.java
+++ src/main/java/MaiOptiks/routing/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    private static final String TITLE = "MyOptiks";
+    private static final String TITLE = "MaiOptics";
 
     @RequestMapping("/")
     public String home(Model model) {
