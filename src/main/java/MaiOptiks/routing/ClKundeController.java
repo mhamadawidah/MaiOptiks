782efc633e98dcfdabbc58cc58580f4ebec8adcc
AlexDieNase <laex62@gmail.com>
2022-09-22 12:44:20 +0200

    wip lagerverwaltung

--- src/main/java/MaiOptiks/routing/ClKundeController.java
+++ src/main/java/MaiOptiks/routing/ClKundeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ClKundeController {
 
     private static final String TITLE = "CL-Kunde";
+    private static final int test = 0;
 
     @RequestMapping("/cl-kunde")
     public String home(Model model) {
