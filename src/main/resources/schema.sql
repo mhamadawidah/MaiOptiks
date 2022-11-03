de49e784ef4630e5e7c600285745fbfb8c9b1a74
Draxler127 <101555446+Draxler127@users.noreply.github.com>
2022-11-03 10:42:48 +0100

    Update schema.sql
    Renamed Nr to ID

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,11 +2,11 @@ CREATE TABLE IF NOT EXISTS Land (LandID varchar(254) NOT NULL, Name varchar(254)
 
 CREATE TABLE IF NOT EXISTS Stadt (PLZ int NOT NULL, Ort varchar(254), Land varchar(254), PRIMARY KEY (PLZ), FOREIGN KEY (Land) REFERENCES Land(LandID));
 
-CREATE TABLE IF NOT EXISTS Krankenkasse (KrankenkassenID varchar(254) NOT NULL, Name varchar(254), Strasse varchar(254), PLZ int, TelefonID varchar(254), Email varchar(254), PRIMARY KEY (KrankenkassenNr), FOREIGN KEY (PLZ) REFERENCES Stadt(PLZ));
+CREATE TABLE IF NOT EXISTS Krankenkasse (KrankenkassenID varchar(254) NOT NULL, Name varchar(254), Strasse varchar(254), PLZ int, TelefonID varchar(254), Email varchar(254), PRIMARY KEY (KrankenkassenID), FOREIGN KEY (PLZ) REFERENCES Stadt(PLZ));
 
-CREATE TABLE IF NOT EXISTS Kunde (KundenID int NOT NULL, Anrede varchar(254), Name varchar(254), Vorname varchar(254), Strasse varchar(254), HausID varchar(254), PLZ Int, Geburtsdatum date, TelefonID varchar(254), Handy varchar(254), EMail varchar(254), KrankenkassenID varchar(254), VersicherungsID varchar(254), Gueltigkeit date, Bemerkung varchar(254),PRIMARY KEY (KundenNr), FOREIGN KEY (PLZ) REFERENCES Stadt(PLZ), FOREIGN KEY (KrankenkassenNr) REFERENCES Krankenkasse(KrankenkassenNr));
+CREATE TABLE IF NOT EXISTS Kunde (KundenID int NOT NULL, Anrede varchar(254), Name varchar(254), Vorname varchar(254), Strasse varchar(254), HausID varchar(254), PLZ Int, Geburtsdatum date, TelefonID varchar(254), Handy varchar(254), EMail varchar(254), KrankenkassenID varchar(254), VersicherungsID varchar(254), Gueltigkeit date, Bemerkung varchar(254),PRIMARY KEY (KundenID), FOREIGN KEY (PLZ) REFERENCES Stadt(PLZ), FOREIGN KEY (KrankenkassenID) REFERENCES Krankenkasse(KrankenkassenID));
 
-CREATE TABLE IF NOT EXISTS Firmenstamm (AugenoptikerIKID varchar(254) NOT NULL, Steuernummer varchar(254), Geschaeftsname varchar(254), Bankverbindung varchar(254), Strasse varchar(254), HausID varchar(254), PLZ int, TelefonID varchar(254), Inhabername varchar(254), Inhabervorname varchar(254), PRIMARY KEY (AugenoptikerIKNr), FOREIGN KEY (PLZ) REFERENCES Stadt(PLZ));
+CREATE TABLE IF NOT EXISTS Firmenstamm (AugenoptikerIKID varchar(254) NOT NULL, Steuernummer varchar(254), Geschaeftsname varchar(254), Bankverbindung varchar(254), Strasse varchar(254), HausID varchar(254), PLZ int, TelefonID varchar(254), Inhabername varchar(254), Inhabervorname varchar(254), PRIMARY KEY (AugenoptikerIKID), FOREIGN KEY (PLZ) REFERENCES Stadt(PLZ));
 
 CREATE TABLE IF NOT EXISTS Mail (EMail varchar(254) NOT NULL, Benutzername varchar(254), Passwort varchar(254), SMTPServer varchar(254), SMTPPort int, SMTPAuthentifizierung bit, PRIMARY KEY (Email));
 
@@ -14,13 +14,13 @@ CREATE TABLE IF NOT EXISTS Abrechnungsart (AbrechnungsartID int NOT NULL, Art va
 
 CREATE TABLE IF NOT EXISTS Gattung (GattungID int NOT NULL, Bezeichnung varchar(254) NOT NULL, PRIMARY KEY (GattungID));
 
-CREATE TABLE IF NOT EXISTS Refraktion (RefraktionID int NOT NULL, MitarbeiterID int, ArztID int, Sph float, Cyl float, Ach float, Adds float, Pris float, Bas float, Visus float, PRIMARY KEY (RefraktionID), FOREIGN KEY (MitarbeiterNr) REFERENCES Mitarbeiter(MitarbeiterNr), FOREIGN KEY (ArztNr) REFERENCES Arzt(ArztNr));
+CREATE TABLE IF NOT EXISTS Refraktion (RefraktionID int NOT NULL, MitarbeiterID int, ArztID int, Sph float, Cyl float, Ach float, Adds float, Pris float, Bas float, Visus float, PRIMARY KEY (RefraktionID), FOREIGN KEY (MitarbeiterID) REFERENCES Mitarbeiter(MitarbeiterID), FOREIGN KEY (ArztID) REFERENCES Arzt(ArztID));
 
-CREATE TABLE IF NOT EXISTS Mitarbeiter (MitarbeiterID int NOT NULL, Name varchar(254), Vorname varchar(254), Starsse varchar(254), HausID varchar(254), PLZ int, TelefonID varchar(254), Handy varchar(254), Email varchar(254), Geburtsdatum date, PRIMARY KEY (MitarbeiterNr), FOREIGN KEY (PLZ) REFERENCES Stadt(PLZ));
+CREATE TABLE IF NOT EXISTS Mitarbeiter (MitarbeiterID int NOT NULL, Name varchar(254), Vorname varchar(254), Starsse varchar(254), HausID varchar(254), PLZ int, TelefonID varchar(254), Handy varchar(254), Email varchar(254), Geburtsdatum date, PRIMARY KEY (MitarbeiterID), FOREIGN KEY (PLZ) REFERENCES Stadt(PLZ));
 
-CREATE TABLE IF NOT EXISTS Arzt (ArztID int NOT NULL, Name varchar(254), Vorname varchar(254), Strasse varchar(254), HausID varchar(254), PLZ int, TelefonID varchar(254), Handy varchar(254), Email varchar(254), PRIMARY KEY (ArztNr), FOREIGN KEY (PLZ) REFERENCES Stadt(PLZ));
+CREATE TABLE IF NOT EXISTS Arzt (ArztID int NOT NULL, Name varchar(254), Vorname varchar(254), Strasse varchar(254), HausID varchar(254), PLZ int, TelefonID varchar(254), Handy varchar(254), Email varchar(254), PRIMARY KEY (ArztID), FOREIGN KEY (PLZ) REFERENCES Stadt(PLZ));
 
-CREATE TABLE IF NOT EXISTS Auftrag (Auftragsnummer int NOT NULL, AbrechnungsartID varchar(254), Rezepturvorhanden bit, Womit varchar(254), Wann varchar(254), Fertig bit, Abgeholt bit, Bezahlt bit, Auftragsbestaetigung varchar(254), Rechnung varchar(254), Mahnung int, KundenID int, Datum date, Werkstatt int, Berater int, Refraktion int, PRIMARY KEY (Auftragsnummer), FOREIGN KEY (KundenNr) REFERENCES Kunde(KundenNr), FOREIGN KEY (Berater) REFERENCES Mitarbeiter(MitarbeiterNr), FOREIGN KEY (Werkstatt) REFERENCES Mitarbeiter(MitarbeiterNr), FOREIGN KEY (Refraktion) REFERENCES Refraktion(RefraktionsNr),FOREIGN KEY (AbrechnungsartID) REFERENCES Abrechnungsart(AbrechnungsartID));
+CREATE TABLE IF NOT EXISTS Auftrag (Auftragsnummer int NOT NULL, AbrechnungsartID varchar(254), Rezepturvorhanden bit, Womit varchar(254), Wann varchar(254), Fertig bit, Abgeholt bit, Bezahlt bit, Auftragsbestaetigung varchar(254), Rechnung varchar(254), Mahnung int, KundenID int, Datum date, Werkstatt int, Berater int, Refraktion int, PRIMARY KEY (Auftragsnummer), FOREIGN KEY (KundenID) REFERENCES Kunde(KundenID), FOREIGN KEY (Berater) REFERENCES Mitarbeiter(MitarbeiterID), FOREIGN KEY (Werkstatt) REFERENCES Mitarbeiter(MitarbeiterID), FOREIGN KEY (Refraktion) REFERENCES Refraktion(RefraktionID),FOREIGN KEY (AbrechnungsartID) REFERENCES Abrechnungsart(AbrechnungsartID));
 
 CREATE TABLE IF NOT EXISTS Farbe (FarbeID int NOT NULL, Bezeichnung varchar(254), Info varchar(254), PRIMARY KEY (FarbeID));
 
@@ -32,14 +32,14 @@ CREATE TABLE IF NOT EXISTS Hornhaut (HornhautID int NOT NULL, RefraktionID int N
 
 CREATE TABLE IF NOT EXISTS Artikelart (ArtID int NOT NULL, Bezeichnung varchar(254), PRIMARY KEY (ArtID));
 
-CREATE TABLE IF NOT EXISTS Artikel (ArtikelID int NOT NULL, Bezeichnung varchar(254), Artikelart varchar(254), Material int, Farbe int, Einkaufspreis float, Verkaufspreis float, Lieferant int, PRIMARY KEY (ArtikelNr), FOREIGN KEY (Material) REFERENCES Material(MaterialID), FOREIGN KEY (Farbe) REFERENCES Farbe(FarbeID), FOREIGN KEY (Lieferant) REFERENCES Lieferant(LieferantID));
+CREATE TABLE IF NOT EXISTS Artikel (ArtikelID int NOT NULL, Bezeichnung varchar(254), Artikelart varchar(254), Material int, Farbe int, Einkaufspreis float, Verkaufspreis float, Lieferant int, PRIMARY KEY (ArtikelID), FOREIGN KEY (Material) REFERENCES Material(MaterialID), FOREIGN KEY (Farbe) REFERENCES Farbe(FarbeID), FOREIGN KEY (Lieferant) REFERENCES Lieferant(LieferantID));
 
-CREATE TABLE IF NOT EXISTS Fassungen (ArtikelID int NOT NULL, GattungID int, Modell varchar(254), Groesse varchar(254), Buegel int, Faktor double, PRIMARY KEY (ArtikelNr), FOREIGN KEY (ArtikelID) REFERENCES Artikel(ArtikelID), FOREIGN KEY (GattungID) REFERENCES Gattung(GattungID));
+CREATE TABLE IF NOT EXISTS Fassungen (ArtikelID int NOT NULL, GattungID int, Modell varchar(254), Groesse varchar(254), Buegel int, Faktor double, PRIMARY KEY (ArtikelID), FOREIGN KEY (ArtikelID) REFERENCES Artikel(ArtikelID), FOREIGN KEY (GattungID) REFERENCES Gattung(GattungID));
 
-CREATE TABLE IF NOT EXISTS Glaeser (ArtikelID int NOT NULL, Werte int, PRIMARY KEY (ArtikelNr), FOREIGN KEY (ArtikelID) REFERENCES Artikel(ArtikelID), FOREIGN KEY (Werte) REFERENCES Refraktion(RefraktionID));
+CREATE TABLE IF NOT EXISTS Glaeser (ArtikelID int NOT NULL, Werte int, PRIMARY KEY (ArtikelID), FOREIGN KEY (ArtikelID) REFERENCES Artikel(ArtikelID), FOREIGN KEY (Werte) REFERENCES Refraktion(RefraktionID));
 
-CREATE TABLE IF NOT EXISTS Kontaktlinsen (ArtikelID int NOT NULL, Werte int, PRIMARY KEY (ArtikelNr), FOREIGN KEY (ArtikelID) REFERENCES Artikel(ArtikelID), FOREIGN KEY (Werte) REFERENCES Refraktion(RefraktionID));
+CREATE TABLE IF NOT EXISTS Kontaktlinsen (ArtikelID int NOT NULL, Werte int, PRIMARY KEY (ArtikelID), FOREIGN KEY (ArtikelID) REFERENCES Artikel(ArtikelID), FOREIGN KEY (Werte) REFERENCES Refraktion(RefraktionID));
 
-CREATE TABLE IF NOT EXISTS Brille (BrillenID int NOT NULL, GlasArtikelIDLinks int, GlasArtikelIDRechts int, FassungsArtikelID int, PRIMARY KEY (BrillenID), FOREIGN KEY (GlasArtikelIDLinks) REFERENCES Artikel (ArtikelNr), FOREIGN KEY (GlasArtikelIDRechts) REFERENCES Artikel(ArtikelNr), FOREIGN KEY (FassungsArtikelID) REFERENCES Artikel(ArtikelNr));
+CREATE TABLE IF NOT EXISTS Brille (BrillenID int NOT NULL, GlasArtikelIDLinks int, GlasArtikelIDRechts int, FassungsArtikelID int, PRIMARY KEY (BrillenID), FOREIGN KEY (GlasArtikelIDLinks) REFERENCES Artikel (ArtikelID), FOREIGN KEY (GlasArtikelIDRechts) REFERENCES Artikel(ArtikelID), FOREIGN KEY (FassungsArtikelID) REFERENCES Artikel(ArtikelID));
 
-CREATE TABLE IF NOT EXISTS Auftragsartikel (AuftragsArtikelID int NOT NULL,AuftragsID int, SehhilfeID int, SehhilfenArt int, PRIMARY KEY (AuftragsArtikelID), FOREIGN KEY (AuftragsNr) REFERENCES Auftrag(Auftragsnummer),FOREIGN KEY (SehhilfeID) REFERENCES Brille(BrillenID));
+CREATE TABLE IF NOT EXISTS Auftragsartikel (AuftragsArtikelID int NOT NULL,AuftragsID int, SehhilfeID int, SehhilfenArt int, PRIMARY KEY (AuftragsArtikelID), FOREIGN KEY (AuftragsID) REFERENCES Auftrag(Auftragsnummer),FOREIGN KEY (SehhilfeID) REFERENCES Brille(BrillenID));
