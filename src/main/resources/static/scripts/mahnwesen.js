c691993f63795a141941057dc52149f7bb8e436d
xderfred <78310976+xderfred@users.noreply.github.com>
2022-12-15 10:42:40 +0100

    Added getKundeByAuftragsNr to Mahnwesen

---
+++ src/main/resources/static/scripts/mahnwesen.js
@@ -0,0 +1,36 @@
+let input_auftragsnr = document.getElementById("auftragsnr");
+let p_kundennummer = document.getElementById("kundennummer");
+let p_kundenname = document.getElementById("kundenname");
+
+let kunde = ""
+
+function getKundeByAuftragsNr() {
+
+
+    doGetRequest(
+        '/api/auftrags/',
+        input_auftragsnr.value,
+        (data) => {   // func
+            console.log("data auftrag", data)
+
+            doGetRequest(
+                '/api/kundes/',
+                data.kundenNr,
+                (data) => {   // func
+                    console.log("data kunde", data)
+
+                    kunde = data.name
+                    console.log("kunde", kunde)
+
+            });
+    });
+
+
+}
+
+
+/*const erstemahnung = `Rechnung Nr. ... vom ...
+                     Sehr geehrte/r ..,
+                     leider haben Sie auf unsere Zahlungserinnerung vom ... nicht reagiert. Wir bitten Sie daher den überfälligen Betrag in Höhe von ... bis zum ... auf unser Konto zu überweisen. Sofern Sie den vorgenannten Termin nicht einhalten, werden wir Ihnen Verzugszinsen und Mahnkosten berechnen müssen.
+                     Sollten Sie zwischenzeitlich bereits Zahlung geleistet haben, betrachten Sie dieses Schreiben bitte als gegenstandslos.
+                     Mit freundlichen Grüßen`*/
