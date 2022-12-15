6f96941cc6e8ef0d55759a987076714b10bd3696
Mhamad Awidah <53232098+mhamadawidah@users.noreply.github.com>
2022-12-15 10:42:08 +0100

    Auftragsartikel und Hornhaut Endpunkte

--- src/main/java/MaiOptiks/repos/AuftragsartikelRepository.java
+++ src/main/java/MaiOptiks/repos/AuftragsartikelRepository.java
@@ -2,7 +2,13 @@ package MaiOptiks.repos;
 
 import MaiOptiks.domain.Auftragsartikel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 
 public interface AuftragsartikelRepository extends JpaRepository<Auftragsartikel, Integer> {
+
+    @Query(value = "SELECT * FROM Auftragsartikel WHERE auftragsNr = :auftragsNr", nativeQuery = true)
+    List<Auftragsartikel> findAllByAuftragsNr(Integer auftragsNr);
 }
