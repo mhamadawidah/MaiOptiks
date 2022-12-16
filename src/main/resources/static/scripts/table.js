fd08a3da3bb7aaa24319ad6fd4a03f03112ee69c
R4mn3t <knystockmarten@gmail.com>
2022-12-16 19:41:40 +0100

    Div Anpassung Adressenverwaltung

--- src/main/resources/static/scripts/table.js
+++ src/main/resources/static/scripts/table.js
@@ -1,6 +1,6 @@
 function createTable(data, containerId, target) {
 	// Bereits eine Tabelle vorhanden?
-	if (document.getElementById(containerId).firstElementChild !== null) {
+	if (document.getElementById(containerId)?.firstElementChild !== null) {
 		const table = document.getElementsByTagName('TABLE')[0];
 		
 		table.remove();
@@ -8,45 +8,49 @@ function createTable(data, containerId, target) {
 		
 		return;
 	} else {
-		const tableContainer = document.getElementById(containerId);
-		const table = document.createElement('TABLE');
-		const tableHead = document.createElement('THEAD');
-		const tableBody = document.createElement('TBODY');
-		const dataCount = data.length;
-		
-		table.appendChild(tableHead);
-		table.appendChild(tableBody);
-		
-		const headerRow = document.createElement('TR');
-		const keys = Object.keys(data[0]);
-		
-		keys
-			.map((item) => {
-				const th = document.createElement('TH');
-				let caps = item.charAt(0).toUpperCase() + item.slice(1);
-				th.innerHTML = caps.valueOf();
-				headerRow.appendChild(th);
-			});
-		// Für jeden Eintrag neue Zeile erstellen
-		for (let i = 0; i < dataCount; i++) {
-			const dataRow = document.createElement('TR');
-			const values = Object.values(data[i]);
-   
-			values
-				.map((item) => {
-					const td = document.createElement('TD');
-					td.innerHTML = item.valueOf();
-                    td.setAttribute('class', 'dataCell');
-					dataRow.appendChild(td);
-				});
-
-			dataRow.setAttribute('id', `${i}`);
+		if (data[0] !== undefined && data[0] !== null) {
+			const tableContainer = document.getElementById(containerId);
+			const table = document.createElement('TABLE');
+			const tableHead = document.createElement('THEAD');
+			const tableBody = document.createElement('TBODY');
+			const dataCount = data.length;
 			
-			tableHead.appendChild(headerRow);
-			tableBody.appendChild(dataRow);
+			table.appendChild(tableHead);
+			table.appendChild(tableBody);
+			
+			const headerRow = document.createElement('TR');
+			const keys = Object.keys(data[0]);
+			
+			keys
+				.map((item) => {
+					const th = document.createElement('TH');
+					let caps = item.charAt(0).toUpperCase() + item.slice(1);
+					th.innerHTML = caps.valueOf();
+					headerRow.appendChild(th);
+				});
+			// Für jeden Eintrag neue Zeile erstellen
+			for (let i = 0; i < dataCount; i++) {
+				const dataRow = document.createElement('TR');
+				const values = Object.values(data[i]);
+				
+				values
+					.map((item) => {
+						const td = document.createElement('TD');
+						td.innerHTML = item.valueOf();
+						td.setAttribute('class', 'dataCell');
+						dataRow.appendChild(td);
+					});
+				
+				dataRow.setAttribute('id', `${i}`);
+				
+				tableHead.appendChild(headerRow);
+				tableBody.appendChild(dataRow);
+			}
+			
+			tableContainer.appendChild(table);
+		} else {
+			alert('\nKeine Daten vorhanden');
 		}
-		
-		tableContainer.appendChild(table);
 	}
 	
 	if (target !== undefined) {
